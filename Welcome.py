1597c5bbef86a1c0c54840680867ffb916849c58
iamjohnnysky <vanpasie20@gmail.com>
2025-07-03 09:02:00 -0700

    Ternary Operator

--- Welcome.py
+++ Welcome.py
@@ -1,7 +1,10 @@
-meaning = 8 
+meaning = 42
 print('')
 
-if meaning > 10:
-    print('Right on!')
-else:
-    print('Not today')    
+# if meaning > 10:
+#     print('Right on!')
+# else:
+#     print('Not today')    
+
+# Ternary Operator
+print('Right On!') if meaning > 10 else print('Not today')
