a0538ce44f1173ff8c4f75607f40119083b78a77
iamjohnnysky <vanpasie20@gmail.com>
2025-07-07 04:01:50 -0700

    Data Types for python

--- data_types.py
+++ data_types.py
@@ -1,3 +1,5 @@
+import math 
+
 #String data type 
 
 # literal assignment 
@@ -61,3 +63,72 @@ print(len(multiline))
 print(len(multiline.strip()))
 print(len(multiline.lstrip()))
 print(len(multiline.rstrip()))
+
+print("")
+
+# Build a menu 
+title = "menu".upper()
+print(title.center(20, "#"))
+print("Coffee".ljust(16, ".") + "$1".rjust(4))
+print("Muffin".ljust(16, ".") + "$2".rjust(4))
+print("Cheesecake".ljust(16, ".") + "$4".rjust(4))
+
+print("")
+
+# String index values
+print(first[1])
+print(first[-1])
+print(first[1:-1])
+print(first[1:])
+
+# Some methods return boolean data
+print(first.startswith("J"))
+print(first.endswith("Z"))
+
+# Boolean data type
+myvalue = True
+x = bool(False)
+print(type(x))
+print(isinstance(myvalue, bool))
+
+# Numeric data types 
+
+# Integer data type
+price = 100
+best_price = int(80)
+print(type(price))
+print(isinstance(best_price, int))
+
+# Float type
+gpa = 3.28
+y = float(1.14)
+print(type(gpa))
+
+# Complex type
+comp_value = 5+3j
+print(type(comp_value))
+print(comp_value.real)
+print(comp_value.imag)
+
+# Built-in functions for numbers
+
+print(abs(gpa))
+print(abs(gpa * -1))
+
+print(round(gpa))
+
+print(round(gpa, 1))
+
+
+print(math.pi)
+print(math.sqrt(64))
+print(math.ceil(gpa))
+print(math.floor(gpa))
+
+# Casting a string to a number
+Zipcode = "10001"
+zip_value = int(Zipcode)
+print(type(zip_value))
+
+# Error if you attempt to cast incorrect data
+# zip_value = int("New York")  
