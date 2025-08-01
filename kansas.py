65c1186d5058666a146ee7ae9ecdb1ca78fe5743
iamjohnnysky <vanpasie20@gmail.com>
2025-08-01 15:47:40 -0700

    creating a Python Modules connected to kansas.py and rps.py

---
+++ kansas.py
@@ -0,0 +1,25 @@
+from random import choice
+
+capital = "Topeka"
+
+bird = "Western Meadowlark"
+
+flower = "Sunflower"
+
+song = "Home on the Range"
+
+
+def randomfunfact():
+    funfacts = [
+         "Kansas is considered flat, but it does have a mountain.",
+         "Wichita is the largest city in the state, but many would guess that it is kansas city.",
+         "A considerable portion of kansas city is actually in Missouri.",
+         "Most kansas are annoyed by wizard of Oz references from people outside of kansas."
+    ]
+
+    index = choice("0123")
+
+    print(funfacts[int(index)])
+
+if __name__ == "__main__":
+    randomfunfact()
